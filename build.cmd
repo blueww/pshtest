7cb345ecb1ace11acd1edeb377ac44b2ebca0d30
Zachary Yang <jiyang@microsoft.com>
2014-08-27 15:19:23 +0800

    Add a build.cmd

---
+++ build.cmd
@@ -0,0 +1,11 @@
+cd azcopy
+msbuild /t:Rebuild /p:Configuration=Debug AzCopy.sln
+cd ..
+
+cd Xplat
+.\tools\windows\build.cmd
+cd ..
+
+cd test
+msbuild /t:Rebuild /p:Configuration=Debug CliTest.sln
+cd ..
