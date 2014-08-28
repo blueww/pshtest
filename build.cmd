365dd00b0aeecd11bd2d9232a923846c93193bec
Vincent Jiang (AZURE TEST) <vinjiang@microsoft.com>
2014-08-27 21:57:10 -0700

    Fix batch build issues to make sure CLITest is built.

--- build.cmd
+++ build.cmd
@@ -2,10 +2,10 @@ cd azcopy
 msbuild /t:Rebuild /p:Configuration=Debug AzCopy.sln
 cd ..
 
-cd Xplat
-.\tools\windows\build.cmd
-cd ..
-
 cd test
 msbuild /t:Rebuild /p:Configuration=Debug CliTest.sln
+cd ..
+
+cd Xplat
+.\tools\windows\build.cmd
 cd ..
