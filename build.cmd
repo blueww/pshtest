9c53683db2c087ca5143409ab0b1a51f5ec9920a
Vincent Jiang (AZURE TEST) <vinjiang@microsoft.com>
2014-08-29 08:02:32 -0700

    Enable PSH MSI build

--- build.cmd
+++ build.cmd
@@ -6,6 +6,10 @@ cd test
 msbuild /t:Rebuild /p:Configuration=Debug CliTest.sln
 cd ..
 
+cd PowerShell\tools
+powershell -File BuildInstaller.ps1
+cd ..\..
+
 cd Xplat
 .\tools\windows\build.cmd
 cd ..
