42c81875e4f18394a49d3720241b35d9f4e3a803
jiyang <jiyang@microsoft.com>
2015-01-29 11:30:55 +0800

    update build and sync batch

--- sync_submodules.cmd
+++ sync_submodules.cmd
@@ -1,7 +1,10 @@
+git submodule init
+git submodule update
+
 echo "update submodule azcopy"
 cd azcopy
 git fetch
-git reset origin/v4.1-pr --hard
+git reset origin/cli_base --hard
 cd ..
 echo "update submodule Powershell"
 cd PowerShell
@@ -14,5 +17,13 @@ git fetch
 git reset origin/next --hard
 cd ..
 
+if [%1] == [JENKINS] GOTO :JENKINS
+
+:LOCAL
 git add -A
 git commit -m "sync submodules to latest"
+
+:JENKINS
+powershell.exe "test\internal\scripts\InjectBuildNumber.ps1"
+git add -A
+git commit -m "dmshbld update submodules and increase build number to %BUILD_NUMBER%"
