6227b98f8419aa3fc2b7c7792eafbeccc916c18a
jiyang <jiyang@microsoft.com>
2015-01-29 11:37:42 +0800

    correct sync batch

--- sync_submodules.cmd
+++ sync_submodules.cmd
@@ -22,8 +22,11 @@ if [%1] == [JENKINS] GOTO :JENKINS
 :LOCAL
 git add -A
 git commit -m "sync submodules to latest"
+GOTO :END
 
 :JENKINS
 powershell.exe "test\internal\scripts\InjectBuildNumber.ps1"
 git add -A
 git commit -m "dmshbld update submodules and increase build number to %BUILD_NUMBER%"
+
+:END
