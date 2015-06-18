e262aa9d448198b1bc854a98d2baac7acba11b0f
emmazhu <emmazhu@microsoft.com>
2015-06-18 14:23:16 +0800

    Add BVT test cases for get file copy state.

--- test/CLITest/NodeJSAgent.cs
+++ test/CLITest/NodeJSAgent.cs
@@ -995,6 +995,16 @@ namespace Management.Storage.ScenarioTest
             throw new NotImplementedException();
         }
 
+        public override bool StopFileCopy(string shareName, string filePath, string copyId, bool force = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool StopFileCopy(CloudFile file, string copyId, bool force = true)
+        {
+            throw new NotImplementedException();
+        }
+
         public override void OutputValidation(Collection<Dictionary<string, object>> comp)
         {
             Test.Info("Validate Dictionary objects");
@@ -1619,81 +1629,6 @@ namespace Management.Storage.ScenarioTest
         public override bool SetAzureStorageShareQuota(CloudFileShare share, int quota)
         {
             return SetAzureStorageShareQuota(share.Name, quota);
-        }
-
-        public override bool StartFileCopyFromBlob(string containerName, string blobName, string shareName, string filePath, object destContext, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopy(CloudBlobContainer container, string blobName, string shareName, string filePath, object destContext, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopy(CloudBlob blob, string shareName, string filePath, object destContext, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopy(CloudBlob blob, CloudFile destFile, object destContext, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopyFromFile(string srcShareName, string srcFilePath, string shareName, string filePath, object destContext, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopy(CloudFileShare share, string srcFilePath, string shareName, string filePath, object destContext, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopy(CloudFileDirectory dir, string srcFilePath, string shareName, string filePath, object destContext, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopy(CloudFile srcFile, string shareName, string filePath, object destContext, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopy(CloudFile srcFile, CloudFile destFile, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopy(string uri, string destShareName, string destFilePath, object destContext, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StartFileCopy(string uri, CloudFile destFile, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool GetFileCopyState(string shareName, string filePath, bool waitForComplete = false)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool GetFileCopyState(CloudFile file, bool waitForComplete = false)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StopFileCopy(string shareName, string filePath, string copyId, bool force = true)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool StopFileCopy(CloudFile file, string copyId, bool force = true)
-        {
-            throw new NotImplementedException();
         }
 	
         public override void AssertNoError()
