d8c2afbe55f811c2a0f84e5dac7313334e3bdb61
dmshbld <dmsh@microsoft.com>
2015-09-22 14:51:37 +0800

    dmshbld update submodules and increase build number to 94

--- test/CLITest/Properties/AssemblyInfo.cs
+++ test/CLITest/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.91")]
+[assembly: AssemblyFileVersion("1.0.0.94")]
 
