4c429e5998fb109df5ba50a445618b3ab0c0d31b
emmazhu <emmazhu@microsoft.com>
2015-10-14 18:37:01 +0800

    Register PS repository to walk away the invoking to New-Guid

--- PublishModules.ps1
+++ PublishModules.ps1
@@ -5,6 +5,16 @@ $PACKAGEPATH=[System.Environment]::GetEnvironmentVariable("PACKAGEPATH")
 
 mkdir $PACKAGEPATH\package
 
+$repo = Get-PSRepository | where { $_.SourceLocation -eq $repositoryLocation }
+
+if ($repo -ne $null) {
+    UnRegister-PSRepository $repo.Name
+} 
+
+$repoName = [System.Guid]::NewGuid().ToString()
+
+Register-PSRepository -Name $repoName -SourceLocation $PACKAGEPATH -PublishLocation $PACKAGEPATH/package -InstallationPolicy Trusted
+
 [System.Environment]::SetEnvironmentVariable("NuGetPublishingSource", $PACKAGEPATH, "Process")
 
 msbuild "$env:AzurePSRoot\build.proj" /t:Publish /p:NuGetKey=a253d1a5-21a2-45a5-ab83-d489611b5dc7
