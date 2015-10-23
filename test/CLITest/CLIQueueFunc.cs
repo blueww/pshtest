0eef1a1a899d19399c1d3b016951be8c0461f87c
emmazhu <emmazhu@microsoft.com>
2015-10-23 14:16:14 +0800

    Fix test issues which caused by that the new error handling will break pipeline when there's one error happened.

--- test/CLITest/CLIQueueFunc.cs
+++ test/CLITest/CLIQueueFunc.cs
@@ -120,16 +120,12 @@ namespace Management.Storage.ScenarioTest
                     // Verification for returned values
                     Test.Assert(agent.Output.Count == 0, "0 row returned : {0}", agent.Output.Count);
 
-                    int i = 0;
-                    foreach (string name in QUEUE_NAMES)
-                    {
-                        Test.Assert(agent.ErrorMessages[i].Equals(String.Format("Queue '{0}' already exists.", name)), agent.ErrorMessages[i]);
-                        ++i;
-                    }
+                    Test.Assert(agent.ErrorMessages[0].Contains(String.Format("Queue '{0}' already exists.", QUEUE_NAMES[0])), agent.ErrorMessages[0]);
 
                     //--------------3. New operation--------------
                     Test.Assert(!agent.NewAzureStorageQueue(PARTLY_EXISTING_NAMES), Utility.GenComparisonData("NewAzureStorageQueue", false));
-                    Test.Assert(agent.Output.Count == 1, "1 row returned : {0}", agent.Output.Count);
+                    Test.Assert(agent.Output.Count == 0, "0 row returned : {0}", agent.Output.Count);
+                    Test.Assert(agent.ErrorMessages[0].Contains(String.Format("Queue '{0}' already exists.", PARTLY_EXISTING_NAMES[0])), agent.ErrorMessages[0]);
                 }
                 else
                 {
@@ -164,7 +160,7 @@ namespace Management.Storage.ScenarioTest
             finally
             {
                 //--------------5. Remove operation--------------
-                Test.Assert(agent.RemoveAzureStorageQueue(MERGED_NAMES), Utility.GenComparisonData("RemoveAzureStorageQueue", true));
+                Test.Assert(agent.RemoveAzureStorageQueue(QUEUE_NAMES), Utility.GenComparisonData("RemoveAzureStorageQueue", true));
                 // Check if all the above queues have been removed
                 foreach (string name in QUEUE_NAMES)
                 {
