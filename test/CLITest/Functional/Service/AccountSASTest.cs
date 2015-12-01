af2b95d9908202ad19f4d3a36a17057c925c602f
Wei Wei <weiwei@microsoft.com>
2015-12-01 10:27:29 +0800

    sync submodules to latest

--- test/CLITest/Functional/Service/AccountSASTest.cs
+++ test/CLITest/Functional/Service/AccountSASTest.cs
@@ -39,6 +39,54 @@ namespace Management.Storage.ScenarioTest.Functional.Service
             blobUtil.ValidateBlobWriteableWithSasToken(blobUtil.Blob, sasToken);
         }
 
+        [TestMethod]
+        [TestCategory(Tag.Function)]
+        public void AccountSASWithRandomParameters()
+        {
+            SharedAccessAccountServices randomService = (SharedAccessAccountServices)TestBase.random.Next(0, 15);
+            SharedAccessAccountResourceTypes randomReourceType = (SharedAccessAccountResourceTypes)TestBase.random.Next(0, 7);
+            string randomPermission = string.Empty;
+
+            foreach (char p in "racwdlup")
+            {
+                if (TestBase.random.NextDouble() >= 0.5)
+                    randomPermission += p;
+            }
+            
+            SharedAccessProtocol? randomProtocol = null;            
+            if (TestBase.random.NextDouble() >= 0.5)
+            {
+                randomProtocol = SharedAccessProtocol.HttpsOnly;
+            }
+            else
+            {
+                randomProtocol = SharedAccessProtocol.HttpsOrHttp;
+            }
+
+            string randomIPAddressOrRange = null;            
+            if (TestBase.random.NextDouble() >= 0.5)
+            {
+                randomIPAddressOrRange = "10.12.233.1";
+            }
+            else
+            {
+                randomIPAddressOrRange = "12.34.56.234-12.157.57.127";
+            }
+
+            DateTime? randomStartTime = DateTime.Now.AddSeconds(-1 * TestBase.random.Next(0, 1000));
+            DateTime? randomExpiryTime = DateTime.Now.AddSeconds(TestBase.random.Next(1000, 100000));
+
+
+            string sasToken = GenerateAndValidateAccountSAS(
+                randomService,
+                randomReourceType,
+                randomPermission, 
+                randomProtocol, 
+                randomIPAddressOrRange, 
+                randomStartTime, 
+                randomExpiryTime);
+        }
+
         private string GenerateAndValidateAccountSAS(
             SharedAccessAccountServices service,
             SharedAccessAccountResourceTypes resourceType,
