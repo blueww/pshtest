5f20f10a1ce4d1dd642a746da5dfd8cf888095b5
emmazhu <emmazhu@microsoft.com>
2015-12-23 15:08:22 +0800

    Publish PowerShell packages to private repository.

--- PublishModules.ps1
+++ PublishModules.ps1
@@ -5,16 +5,14 @@ $PACKAGEPATH=[System.Environment]::GetEnvironmentVariable("PACKAGEPATH")
 
 mkdir $PACKAGEPATH\package
 
-$repo = Get-PSRepository | where { $_.SourceLocation -eq $repositoryLocation }
-
-if ($repo -ne $null) {
-    UnRegister-PSRepository $repo.Name
-} 
-
 $repoName = [System.Guid]::NewGuid().ToString()
 
 Register-PSRepository -Name $repoName -SourceLocation $PACKAGEPATH -PublishLocation $PACKAGEPATH/package -InstallationPolicy Trusted
 
 [System.Environment]::SetEnvironmentVariable("NuGetPublishingSource", $PACKAGEPATH, "Process")
 
+"$env:AzurePSRoot\tools\PublishModules.ps1" Debug ServiceManagement a253d1a5-21a2-45a5-ab83-d489611b5dc7 $PACKAGEPATH
+
 msbuild "$env:AzurePSRoot\build.proj" /t:Publish /p:NuGetKey=a253d1a5-21a2-45a5-ab83-d489611b5dc7
+
+UnRegister-PSRepository -Name $repoName
