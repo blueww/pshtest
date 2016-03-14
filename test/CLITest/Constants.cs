adda9d3a65dcf69101a9da51d93db7e3e492375d
emmazhu <emmazhu@microsoft.com>
2016-03-14 11:39:00 +0800

    Change cases according to the change in PSH product to keep cmdlets consistant.

--- test/CLITest/Constants.cs
+++ test/CLITest/Constants.cs
@@ -25,11 +25,11 @@ namespace Management.Storage.ScenarioTest
 
         public struct AccountType
         {
-            public const string Standard_LRS = "Standard_LRS";
-            public const string Standard_ZRS = "Standard_ZRS";
-            public const string Standard_GRS = "Standard_GRS";
-            public const string Standard_RAGRS = "Standard_RAGRS";
-            public const string Premium_LRS = "Premium_LRS";
+            public const string Standard_LRS = "StandardLRS";
+            public const string Standard_ZRS = "StandardZRS";
+            public const string Standard_GRS = "StandardGRS";
+            public const string Standard_RAGRS = "StandardRAGRS";
+            public const string Premium_LRS = "PremiumLRS";
         };
 
         public static readonly string[] AccountTypes = { AccountType.Standard_LRS, 
