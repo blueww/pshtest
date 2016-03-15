ee6f09bdf2ffc35155ea8582b7d5aff545164a3f
emmazhu <emmazhu@microsoft.com>
2016-03-15 15:38:01 +0800

    Clean up tempory package folder after build.

--- PublishModules.ps1
+++ PublishModules.ps1
@@ -16,3 +16,9 @@ Register-PSRepository -Name $repoName -SourceLocation $PACKAGEPATH -PublishLocat
 msbuild "$env:AzurePSRoot\build.proj" /t:Publish /p:NuGetKey=a253d1a5-21a2-45a5-ab83-d489611b5dc7
 
 UnRegister-PSRepository -Name $repoName
+
+$FileExists = Test-Path $PACKAGEPATH\0package0
+
+if ($FileExists -eq $True) {
+    del -r -force $PACKAGEPATH\0package0
+}
