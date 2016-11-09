9586dbe1a4e53ddd6569dfc3b106885ce72465fd
Wei Wei <weiwei@microsoft.com>
2016-11-09 09:54:28 +0800

    Fix the build of PowerShell since PowerShell build framework change

--- PublishModules.ps1
+++ PublishModules.ps1
@@ -1,24 +1,13 @@
-$scriptFolder = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-. ($scriptFolder + '.\PowerShell\Tools\SetupEnv.ps1')
 
 $PACKAGEPATH=[System.Environment]::GetEnvironmentVariable("PACKAGEPATH")
 
 mkdir $PACKAGEPATH\package
 
-copy -r $PACKAGEPATH\..\..\0package0 $PACKAGEPATH
-
-$repoName = [System.Guid]::NewGuid().ToString()
-
-Register-PSRepository -Name $repoName -SourceLocation $PACKAGEPATH -PublishLocation $PACKAGEPATH/package -InstallationPolicy Trusted
-
-[System.Environment]::SetEnvironmentVariable("NuGetPublishingSource", $PACKAGEPATH, "Process")
-
-msbuild "$env:AzurePSRoot\build.proj" /t:Publish /p:NuGetKey=a253d1a5-21a2-45a5-ab83-d489611b5dc7
-
-UnRegister-PSRepository -Name $repoName
-
-$FileExists = Test-Path $PACKAGEPATH\0package0
-
-if ($FileExists -eq $True) {
-    del -r -force $PACKAGEPATH\0package0
+# Add nuget.exe path to current path as .\PowerShell\tools\PublishModules.ps1 need it
+$nugetPath = Convert-Path .\PowerShell\tools;
+if ($env:Path -notcontains $nugetPath)
+{
+    $env:Path = $env:Path + ";" + ($nugetPath)
 }
+
+.\PowerShell\tools\PublishModules.ps1 debug All a253d1a5-21a2-45a5-ab83-d489611b5dc7 $PACKAGEPATH
