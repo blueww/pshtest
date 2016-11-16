47e3cb3f40995a1f68f2b590acf1c4349a445166
Wei Wei <weiwei@microsoft.com>
2016-11-16 17:54:05 +0800

    Fix a build script issue

--- PublishModules.ps1
+++ PublishModules.ps1
@@ -3,11 +3,17 @@ $PACKAGEPATH=[System.Environment]::GetEnvironmentVariable("PACKAGEPATH")
 
 mkdir $PACKAGEPATH\package
 
+Pushd PowerShell\Tools
+
 # Add nuget.exe path to current path as .\PowerShell\tools\PublishModules.ps1 need it
-$nugetPath = Convert-Path .\PowerShell\tools;
-if ($env:Path -notcontains $nugetPath)
+$nugetPath = Convert-Path .;
+if ($env:Path -notlike "*" + $nugetPath + "*")
 {
     $env:Path = $env:Path + ";" + ($nugetPath)
 }
+echo "$env:Path: " + $env:Path 
 
-.\PowerShell\tools\PublishModules.ps1 debug All a253d1a5-21a2-45a5-ab83-d489611b5dc7 $PACKAGEPATH
+.\PublishModules.ps1 debug All a253d1a5-21a2-45a5-ab83-d489611b5dc7 $PACKAGEPATH
+
+popd
+popd
