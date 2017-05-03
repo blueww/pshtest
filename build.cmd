7ea01bcdccb0dffc773ee31ab07bba3eef4b4c25
Wei Wei <weiwei@microsoft.com>
2017-05-03 10:06:42 +0800

    eanble generate help in build PSH

--- build.cmd
+++ build.cmd
@@ -11,7 +11,7 @@ if [%PRODUCT%] == [PSH] GOTO :PSH
 cd PowerShell
 # Comment the Build Clean step to avoid the build failure issue in IotHub.Test. Will add it back after OneSDK team fix the issue
 # msbuild build.proj /t:Clean
-msbuild build.proj /t:Build /p:SkipHelp=True 
+msbuild build.proj /t:Build
 IF %ERRORLEVEL% NEQ 0 Exit /B %ERRORLEVEL%
 cd ..
 powershell -NonInteractive -NoLogo -NoProfile -File PublishModules.ps1
