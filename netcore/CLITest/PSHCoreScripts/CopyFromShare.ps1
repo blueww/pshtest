6f12399d142c540ea08c32313a3da37722652882
Emma Zhu <emmazhu@microsoft.com>
2018-09-10 11:19:40 +0800

    Change to reference to renamed NetCore cmdlets.

---
+++ netcore/CLITest/PSHCoreScripts/CopyFromShare.ps1
@@ -0,0 +1,97 @@
+﻿param ([string]$sourceConnectionString,
+[string]$destConnectionString,
+[string]$sourceShareName,
+[string]$destShareName)
+
+
+$sourceCtx = New-AzureStorageContext -ConnectionString $sourceConnectionString
+$destCtx = New-AzureStorageContext -ConnectionString $destConnectionString
+
+$sourceFiles = Get-AzureStorageFile -ShareName $sourceShareName -Context $sourceCtx
+
+function EnsureDirStructure ($dir)
+{
+    if ($dir.Exists() -ne $true)
+    {
+        if ( $dir.Parent -ne $null )
+        {
+            EnsureDirStructure ( $dir.Parent );
+        }
+
+        $dir.Create();
+    }
+}
+
+function GetDirReference ($dirPath, $shareRef)
+{
+    $dir = $shareRef.GetRootDirectoryReference();
+
+    $lastIndex = 0;
+
+    $separatorIndex = $dirPath.IndexOf("/");
+
+    While ($separatorIndex -ne -1)
+    {
+        $dir = $dir.GetDirectoryReference($dirPath.SubString($lastIndex, $separatorIndex - $lastIndex));
+        $lastIndex = $separatorIndex + 1;
+
+        $separatorIndex = $dirPath.IndexOf("/", $lastIndex);
+    }
+
+    return $dir;
+}
+
+function GetDirFullPath($dir)
+{
+    $fullPath = "";
+    if ($dir.Parent -ne $null)
+    {
+        $fullPath = GetDirFullPath($dir.Parent);
+    }
+
+    if ($fullPath -ne "")
+    {
+        return $fullPath + "/" + $dir.Name;
+    }
+    else
+    {
+        return $dir.Name;
+    }
+}
+
+function GetFullPath($file)
+{
+    $dirFullPath = GetDirFullPath($file.Parent);
+
+    if ($dirFullPath -ne "")
+    {
+        return $dirFullPath + "/" + $file.Name;
+    }
+    else
+    {
+        return $file.Name;
+    }
+}
+
+$share = Get-AzureStorageShare -Name $destShareName -Context $destCtx;
+
+function CopyFromFile($sourceFile)
+{ 
+    if ( $_.GetType().Name -eq "CloudFile" )
+    {
+        $destPath = GetFullPath($_);
+
+        try
+        {
+            $dir = GetDirReference $destPath $share;
+            EnsureDirStructure $dir;
+            Start-AzureStorageFileCopy -SrcFile $_ -DestShareName $destShareName -DestFilePath $destPath -DestContext $destCtx -Force;
+        }
+        catch
+        {            
+            Write-Error "Failed to copy to " + $destPath + ": " + $_.Exception.Message;
+        }
+    }
+}
+
+$sourceFiles | %{ CopyFromFile $_ }
